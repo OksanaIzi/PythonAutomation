17c19dc1c6558e3858c76071b9dc45bed2fc6cd4
OksanaIzi <73961325+OksanaIzi@users.noreply.github.com>
2020-11-18 14:33:22 +0400

    добавлена параметризация дле тестов

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,15 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo', ["0","1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail(reason="some bug")), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
-    time.sleep(15)
+    time.sleep(3)
     product_page.should_messages_after_adding_to_basket()
     product_page.should_right_name_of_book()
     product_page.should_right_total_price_of_basket()
